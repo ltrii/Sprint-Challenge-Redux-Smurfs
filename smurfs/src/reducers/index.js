585a91e0eca5d1420de4b06baf89f1f5aad223b8
ltrii <lelandtr@gmail.com>
2019-02-01 08:09:51 -0800

    Init

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,18 +2,18 @@
   Be sure to import in all of the action types from `../actions`
 */
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
+const initialState = {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
+   fetchingSmurfs: false,
+   smurfsFetched: false,
+   addingSmurf: false,
+   smurfAdded: false,
+   updatingSmurf: false,
+   smurfUpdated: false,
+   deletingSmurf: false,
+   smurfDeleted: false,
    error: null
- }
-*/
-
+}
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
@@ -21,3 +21,61 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+export const rootReducer = (state = initialState, action) => {
+  switch(action.type) {
+      case FETCHING_SMURFS:
+          return {
+              ...state,
+              error: '',
+              isFetching: true
+          };
+      case SMURFS_FETCHED:
+          return {
+              ...state,
+              isFetching: false,
+              smurfs: action.payload,
+              error: ''
+          };
+      case ADDING_SMURF:
+          return {
+              ...state,
+              friends: action.payload,
+              isSaving: false,
+              error: ''
+          };
+      case SMURF_ADDED:
+          return {
+              ...state,
+              isSaving: true,
+              error: ''
+          };
+      case UPDATING_SMURF:
+          return {
+              ...state,
+              isUpdating: true,
+              error: ''
+          };
+      case SMURF_UPDATED:
+          return {
+              ...state,
+              friends: action.payload,
+              isUpdating: false,
+              error: ''
+          };
+      case DELETING_SMURF:
+          return {
+              ...state,
+              isDeleting: true,
+              error: ''
+          };
+      case SMURF_DELETED:
+          return {
+              ...state,
+              friends: action.payload,
+              isDeleting: false,
+              error: ''
+          };
+      default:
+          return state;
+      }
+}
